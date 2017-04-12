d6234d0b14456b09ea8528ccd7986bf7917adbce
Evgeny Safronov <division494@gmail.com>
2017-04-12 19:54:17 +0300

    refactor: box elimination

--- src/monitoring.rs
+++ src/monitoring.rs
@@ -1,7 +1,7 @@
 use std::io;
 use std::net::SocketAddr;
 
-use futures::{future, Future};
+use futures::future;
 
 use tokio_core::reactor::Handle;
 use tokio_service::Service;
@@ -18,7 +18,7 @@ impl Service for Handler {
     type Request  = Request;
     type Response = Response;
     type Error    = hyper::Error;
-    type Future   = Box<Future<Item=Response, Error=Self::Error>>;
+    type Future   = future::FutureResult<Self::Response, Self::Error>;
 
     fn call(&self, req: Self::Request) -> Self::Future {
         let res = match (req.method(), req.path()) {
@@ -26,7 +26,7 @@ impl Service for Handler {
             (..) => Response::new().with_status(StatusCode::NotFound),
         };
 
-        box future::ok(res)
+        future::ok(res)
     }
 }
 
