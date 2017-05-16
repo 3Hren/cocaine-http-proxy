d84676edc59c06a1eb4b83ecbd0782a5feeb4fb6
Evgeny Safronov <division494@gmail.com>
2017-05-17 00:55:02 +0300

    refactor: improve background

--- src/background/mod.rs
+++ src/background/mod.rs
@@ -1,3 +1,24 @@
+use std::cmp;
+use std::io;
+use std::time::Duration;
+
+use futures::{Async, Future, Poll};
+
+use tokio_core::reactor::{Handle, Timeout};
+
+const MAX_ATTEMPTS: u32 = 18;
+const MAX_DURATION_SECS: u32 = 32;
+
+/// A value that must be returned from the future that is wrapped with `ExponentialBackOff`.
+///
+/// Passing a `Terminate` will terminate the wrapper future, passing result through. The `Repeat`
+/// value will not terminate the future, instead a new future will be created from the factory and
+/// the computation will go on.
+pub enum RepeatResult<T> {
+    Repeat,
+    Terminate(T),
+}
+
 enum State<F> {
     Fetch(F),
     Retry(Timeout),
@@ -6,7 +27,21 @@ enum State<F> {
 pub trait FutureFactory {
     type Item: Future;
 
+    /// Constructs a new future.
+    ///
+    /// This is called before each retry attempt.
     fn create(&mut self) -> Self::Item;
+}
+
+impl<F, T> FutureFactory for F
+    where F: FnMut() -> T,
+          T: Future
+{
+    type Item = T;
+
+    fn create(&mut self) -> Self::Item {
+        (*self)()
+    }
 }
 
 /// Wraps a future into an another infinite future that will repeatedly retry on error using
@@ -23,9 +58,9 @@ pub struct ExponentialBackOff<T, F> {
     timeout_limit: Duration,
 }
 
-impl<T, F> ExponentialBackOff<T, F>
+impl<T, F, R> ExponentialBackOff<T, F>
     where T: FutureFactory<Item=F>,
-          F: Future<Item=(), Error=()>
+          F: Future<Item=RepeatResult<R>, Error=()>
 {
     pub fn new(mut factory: T, handle: Handle) -> Self {
         let future = factory.create();
@@ -35,20 +70,20 @@ impl<T, F> ExponentialBackOff<T, F>
             state: Some(State::Fetch(future)),
             handle: handle,
             attempts: 0,
-            timeout_limit: Duration::new(32, 0),
+            timeout_limit: Duration::new(MAX_DURATION_SECS, 0),
         }
     }
 
-    fn next(&self) -> Duration {
+    fn next_duration(&self) -> Duration {
         // Hope that 2**18 seconds, which is ~3 days, fits everyone needs.
-        let exp = cmp::min(self.attempts, 18);
+        let exp = cmp::min(self.attempts, MAX_ATTEMPTS);
         let duration = Duration::new(2u64.pow(exp), 0);
 
         cmp::min(duration, self.timeout_limit)
     }
 
-    fn retry_later(&mut self) -> Poll<(), io::Error> {
-        let timeout = Timeout::new(self.next(), &self.handle)?;
+    fn retry_later(&mut self) -> Poll<R, io::Error> {
+        let timeout = Timeout::new(self.next_duration(), &self.handle)?;
 
         self.attempts += 1;
         self.state = Some(State::Retry(timeout));
@@ -56,23 +91,26 @@ impl<T, F> ExponentialBackOff<T, F>
     }
 }
 
-impl<T, F> Future for ExponentialBackOff<T, F>
+impl<T, F, R> Future for ExponentialBackOff<T, F>
     where T: FutureFactory<Item=F>,
-          F: Future<Item=(), Error=()>
+          F: Future<Item=RepeatResult<R>, Error=()>
 {
-    type Item = ();
+    type Item = R;
     type Error = io::Error;
 
     fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
         match self.state.take().unwrap() {
             State::Fetch(mut future) => {
                 match future.poll() {
-                    Ok(Async::Ready(..)) => {
+                    Ok(Async::Ready(RepeatResult::Repeat)) => {
                         self.attempts = 0;
                         return self.retry_later();
                     }
+                    Ok(Async::Ready(RepeatResult::Terminate(v))) => {
+                        return Ok(Async::Ready(v));
+                    }
                     Ok(Async::NotReady) => self.state = Some(State::Fetch(future)),
-                    Err(..) => return self.retry_later(),
+                    Err(()) => return self.retry_later(),
                 }
             }
             State::Retry(mut timeout) => {
